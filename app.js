c5573629e759c7448688c0136682a54a2adc88e7
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2017-11-22 20:57:05 +0700

    Added /send routing and api definition.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const admin = require('firebase-admin');
 const serviceAccount  = require('./chat-service-24935-firebase-adminsdk-dx7x3-f12d8c6b0d.json');
 admin.initializeApp({
@@ -9,20 +12,25 @@ admin.initializeApp({
     databaseURL: "https://chat-service-24935.firebaseio.com"
 });
 
-//Send Messages
-var registrationToken = "cbyyqBPF8YA:APA91bHIDCIfBSTrVUl2AxwFdIbvoWClsxIygyBOfNoYqqX78vJHs4YFG2VH4t-x8s0N4TItZsaA3Sjd30Nl822i63ap-qeUBal2D7YwUampM3PIUM11EDrgLsQNuc1OZ8Vbe-yZpD5_";
-var payload = {
-    data: {
-        text: "Testing messages"
-    }
-};
-
-admin.messaging().sendToDevice(registrationToken, payload)
-.then(function (response) {
-    console.log("Successfully sent message: ", response);
-})
-.catch(function (error) {
-    console.log("Error sending message: ", error);
+//Routing
+app.post('/send', function (req, res) {
+    let registrationToken = req.body.firebaseToken;
+    let payload = req.body.payload;
+    sendMessage(registrationToken, payload);
+    console.log(req.body);
+    res.send("Testing.");
 });
+
+
+//Functions
+function sendMessage(registrationToken, payload) {
+    admin.messaging().sendToDevice(registrationToken, payload)
+        .then(function (response) {
+            console.log("Successfully sent message: ", response);
+        })
+        .catch(function (error) {
+            console.log("Error sending message: ", error);
+        });
+}
 
 app.listen(3000, function () {console.log("Listening on port 3000")} );
