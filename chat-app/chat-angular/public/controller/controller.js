404c105902fe7b2f31fe29bbb64033601f97b815
Muhammad Fathur Rahman <mfathur2707@gmail.com>
2017-11-26 19:30:28 +0900

    tambah dikit

--- chat-app/chat-angular/public/controller/controller.js
+++ chat-app/chat-angular/public/controller/controller.js
@@ -10,6 +10,9 @@ myApp.controller('AppView', ['$rootScope','$scope', '$http', '$location',
         var sender_id = $location.search().sender_id;
         var receiver_id = $location.search().receiver_id;
 
+
+        $scope.sid = sender_id;
+            
         $http.get('http://localhost:3000/chat/chatapp/' + sender_id + "/" + receiver_id).then(function (response) {
           console.log("i got the data i requested");    
           $scope.chats = response.data;  
