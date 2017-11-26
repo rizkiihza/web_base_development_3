94880c5e3c5ccdba2a7924f46aea0a3f213b9791
pram <rizki.ihza@gmail.com>
2017-11-26 14:17:32 +0700

    hapus angular.js

--- chat-app/chat-angular/angular.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use('/chat?',express.static(__dirname + "/public"));
-
-app.listen(4000, function() {
-    console.log("success connect to port 4000");
-});
