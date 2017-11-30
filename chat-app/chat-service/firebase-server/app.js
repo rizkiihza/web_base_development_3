39d758cf7859f21f537ec96152f8152b9cb5e76b
pram <rizki.ihza@gmail.com>
2017-11-30 23:44:56 +0700

    ah kelar

--- chat-app/chat-service/firebase-server/app.js
+++ chat-app/chat-service/firebase-server/app.js
@@ -4,40 +4,46 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const admin = require('firebase-admin');
-const serviceAccount  = require('./chat-service-24935-firebase-adminsdk-dx7x3-f12d8c6b0d.json');
+const serviceAccount  = require('./chat-app-wbd3-firebase-adminsdk-pa77q-51c55e1141.json');
 const http = require('http');
 const request = require('request');
+const cors = require("cors");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://chat-service-24935.firebaseio.com"
+    databaseURL: "https://chat-app-wbd3.firebaseio.com"
 });
+
+app.use(cors());
 
 //Routing
 app.post('/send', function (req, res, next) {
-    //API tokenapp digunakan untuk mendapatkan token firebase 
-    // var url='http://localhost:3030/token/tokenapp/' + req.body.user_id;
+    // API tokenapp digunakan untuk mendapatkan token firebase 
+    var url='http://localhost:3030/token/tokenapp/' + req.body.user_id;
     
-    //request ke API tokenapp
-    // request.get(url, (error, response, body) => {
-    //     if (error) {
-    //         res.json({"msg": error});
-    //         console.log(error);
-    //     }
+    // request ke API tokenapp
+    request.get(url, (error, response, body) => {
+        if (error) {
+            res.json({"msg": error});
+            console.log(error);
+        }
 
-    //     //parsing hasil dari get request
-    //     let token_object = JSON.parse(body);
-    //     console.log(token_object);
+        //parsing hasil dari get request
+        let token_object = JSON.parse(body);
+        console.log(token_object);
 
-    //     //proses untuk mengrim pesan
-    //     let registrationToken = token_object[0].token;
-    //     let payload = req.body.payload;
-    //     sendMessage(registrationToken, payload);
-    // })
-    let registrationToken = req.body.firebaseToken;
-    let payload = req.body.payload;
-    sendMessage(registrationToken, payload);
-    res.json({msg : "success"});
+        //proses untuk mengrim pesan
+        let registrationToken = token_object[0].token;
+        let payload = req.body.payload;
+        sendMessage(registrationToken, payload);
+
+        //memberi respon success
+        res.json({"msg": "success"});
+    })
+    // let registrationToken = req.body.firebaseToken;
+    // let payload = req.body.payload;
+    // sendMessage(registrationToken, payload);
+    // res.json({msg : "success"});
 });
 
 
